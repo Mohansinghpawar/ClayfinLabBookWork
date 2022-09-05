2dadb2e177a09b5776b0d7300b21d9b1ed996782
Mohansinghpawar <88230569+Mohansinghpawar@users.noreply.github.com>
2022-09-05 14:47:26 +0530

    Create LabBook_2.3.java

---
+++ LabBook_2.3.java
@@ -0,0 +1,32 @@
+public class Person {
+	private String name;
+	private int perId;
+	public Person(String name, int perId) {
+		super();
+		this.name = name;
+		this.perId = perId;
+		System.out.println("Argumented Countructor...");
+	}
+	public Person() {
+		System.out.println("no-argument counstrctor...");
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPerId() {
+		return perId;
+	}
+	public void setPerId(int perId) {
+		this.perId = perId;
+	}
+	public static void main(String[] args) {
+         Person person=new Person("mohan",1002);
+         person.getPerId();
+         person.getName();
+	}
+
+}
+
